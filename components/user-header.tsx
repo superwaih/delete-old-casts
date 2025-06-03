48b45692fe0a098376545ccf2ff471166ab01cea
Adewale shittu <sheriffhardewale@gmail.com>
2025-06-03 12:33:08 +0100

    fix: adjust layout and styling of user header component

--- components/user-header.tsx
+++ components/user-header.tsx
@@ -14,21 +14,23 @@ type UserHeaderProps = {
 
 export default function UserHeader({ user }: UserHeaderProps) {
   return (
-    <header className="w-full max-w-md mx-auto mt-10 bg-white shadow-md rounded-xl p-6 text-center">
+    <header className="w-full flex max-w-md gap-4  m-2 bg-white shadow-md rounded-xl p-2">
       {user.pfpUrl && (
         <img
-          src={user.pfpUrl}
+          src={user?.pfpUrl}
           alt="Profile Picture"
-          className="w-24 h-24 rounded-full object-cover mx-auto mb-4 border border-gray-200"
+          className="w-12 h-12 rounded-full object-cover mx-auto mb-4 border border-gray-200"
         />
       )}
-      <h1 className="text-2xl font-semibold text-gray-800">
-        Welcome, {user.displayName || user.username || "User"}
-      </h1>
-      <div className="mt-2 text-gray-600">
-        <p>FID: {user.fid}</p>
-        {user.username && <p>Username: {user.username}</p>}
-        {user.displayName && <p>Display Name: {user.displayName}</p>}
+      <div>
+        <h1 className="text-2xl font-semibold text-gray-800">
+          Welcome, {user.displayName || user.username || "User"}
+        </h1>
+        <div className="mt-2 text-gray-600">
+          <p>FID: {user.fid}</p>
+          {user.username && <p>Username: {user.username}</p>}
+          {user.displayName && <p>Display Name: {user.displayName}</p>}
+        </div>
       </div>
     </header>
   );
